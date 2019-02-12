a81add091641ebc80d50dfd4d6ecc0f2b8779b0d
Matěj Kneifl <matej.kneifl@seznam.cz>
2019-02-12 15:20:15 +0100

    Update index.php
    A IHOTOVO >://///////\

--- index.php
+++ index.php
@@ -4,26 +4,28 @@
     <meta charset="utf-8">
     <title>::)</title>
     <style media="screen">
+      .b {
+        text-align: center;
+      }
       img {
-        height: 400px;
         width: 400px;
-        border-style: solid;
-        border: 10px;
+        height: 400px;
+        display: inline-block;
       }
     </style>
   </head>
   <body>
+   <div class="b">
     <?php
-    define("ROZ", 5);
-    $a = ROZ;
-      for ($y=0; $y < 5 ; $y++) {
-        for ($x=0; $x < $a; $x++) {
-
-          echo "<img src='brick.jpg' alt='aaa'>";
+    $a = 1;
+      for ($y=0; $y < 5; $y++) {
+       for ($x=0; $x < $a; $x++) {
+        ?> <img src='brick.jpg' alt='aaa'> <?php
         }
-            $a--;
+          $a++;
           echo "<br>";
       }
     ?>
+      </div>
   </body>
 </html>
